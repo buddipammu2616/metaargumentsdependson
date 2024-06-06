79ad02385d44fa32d99376029b4ed4a9658a3218
mahesh <maheshkuraganti9@gmail.com>
2024-06-06 15:32:53 +0530

    again updated and commited

--- vpcmanifest/elasticip.tf
+++ vpcmanifest/elasticip.tf
@@ -1,6 +1,6 @@
 #Create Elastic ip
 resource "aws_eip" "maheshEIP" {
   instance = aws_instance.maheshec2.id
-   domain = Vpc
+   domain = "vpc"
   depends_on = [ aws_internet_gateway.maheshIGW ]
 }
