b562e0af7278ba4f04c7cdcc025afec106e9dcaa
mahesh <maheshkuraganti9@gmail.com>
2024-06-06 16:01:32 +0530

    again commited

--- vpcmanifest/ec2.tf
+++ vpcmanifest/ec2.tf
@@ -1,20 +1,19 @@
 #Create a EC2 instance
 resource "aws_instance" "maheshec2" {
-  ami = "ami-00fa32593b478ad6e"
-  instance_type = "t2.micro"
-  key_name = "terraform-keypair"
-  subnet_id = aws_subnet.maheshpublicsubnet.id
-  user_data = <<-EOF
-    #!/bin/bash
-    sudo yum update -y
-    sudo yum install httpd -y
-    sudo systemctl enable httpd
-    sudo systemctl start httpd
-    echo "<h1>Welcome to StackSimplify ! AWS Infra created using Terraform in us-east-1 Region</h1>" > /var/www/html/index.html
-     EOF  
-  vpc_security_group_ids = [aws_security_group.maheshSG.id]
-  #user_data = file("apache-install.sh")
-  tags = {
-    "Name" = "maheshec2"
-  }
+    ami = "ami-00fa32593b478ad6e"
+    instance_type = "t2.micro"
+    subnet_id = aws_subnet.maheshpublicsubnet
+    key_name = "terraform-keypair"
+    user_data = <<-EOF
+     #! /bin/bash
+sudo yum update -y
+sudo yum install -y httpd
+sudo service httpd start
+sudo systemctl enable httpd
+echo "<h1> Hi MAHESH </h1>" > /var/www/html/index.html
+EOF
+vpc_security_group_ids = [aws_security_group.maheshSG]
+ tags = {
+   "Name"= "maheshec2"
+ }
 }
