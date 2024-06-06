5e3846451d0992c8e6e13d14b492e1a1c1c39843
mahesh <maheshkuraganti9@gmail.com>
2024-06-06 15:21:01 +0530

    some content added in apache install sh and commited

--- vpcmanifest/apache-install.sh
+++ vpcmanifest/apache-install.sh
@@ -1,5 +1,6 @@
+#! /bin/bash
 sudo yum update -y
 sudo yum install -y httpd
 sudo service httpd start
 sudo systemctl enable httpd
-echo "<h1> Hi MAHESH"</h1>
+echo "<h1> Hi MAHESH </h1>" > /var/www/html/index.html
