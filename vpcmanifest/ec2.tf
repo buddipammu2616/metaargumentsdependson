d377cc012ce473e160c3d5cee42298a455bc9a42
mahesh <maheshkuraganti9@gmail.com>
2024-06-06 15:46:09 +0530

    ec2 updatead and commited

--- vpcmanifest/ec2.tf
+++ vpcmanifest/ec2.tf
@@ -4,8 +4,16 @@ resource "aws_instance" "maheshec2" {
   instance_type = "t2.micro"
   key_name = "terraform-keypair"
   subnet_id = aws_subnet.maheshpublicsubnet.id
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo yum update -y
+    sudo yum install httpd -y
+    sudo systemctl enable httpd
+    sudo systemctl start httpd
+    echo "<h1>Welcome to StackSimplify ! AWS Infra created using Terraform in us-east-1 Region</h1>" > /var/www/html/index.html
+     EOF  
   vpc_security_group_ids = [aws_security_group.maheshSG.id]
-  user_data = file("apache-install.sh")
+  #user_data = file("apache-install.sh")
   tags = {
     "Name" = "maheshec2"
   }
