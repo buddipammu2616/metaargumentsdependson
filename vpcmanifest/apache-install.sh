798dbc3fbd3442515ca380fc1bf708bc684fc55c
mahesh <maheshkuraganti9@gmail.com>
2024-06-06 16:37:43 +0530

    apcahe install commited

--- vpcmanifest/apache-install.sh
+++ vpcmanifest/apache-install.sh
@@ -3,5 +3,4 @@ sudo yum update -y
 sudo yum install -y httpd
 sudo service httpd start
 sudo systemctl enable httpd
-echo "<h1> Hi MAHESH </h1>" > /var/www/html/index.html
-echo "<h1>Welcome to StackSimplify ! AWS Infra created using Terraform in ap-south-1 Region</h1>" > /var/www/html/index.html
+echo "<h1>Hi MAHESH</h1>" > /var/www/html/index.html
