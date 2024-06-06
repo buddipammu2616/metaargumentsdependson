23280530ffbe8dc04a68d002511beddedc66034e
mahesh <maheshkuraganti9@gmail.com>
2024-06-06 16:20:58 +0530

    vpc commited

--- vpcmanifest/ec2.tf
+++ vpcmanifest/ec2.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "maheshec2" {
     ami = "ami-00fa32593b478ad6e"
     instance_type = "t2.micro"
-    subnet_id = aws_subnet.maheshpublicsubnet
+    subnet_id = aws_subnet.maheshpublicsubnet.id
     key_name = "terraform-keypair"
     user_data = <<-EOF
      #! /bin/bash
@@ -12,7 +12,7 @@ sudo service httpd start
 sudo systemctl enable httpd
 echo "<h1> Hi MAHESH </h1>" > /var/www/html/index.html
 EOF
-vpc_security_group_ids = [aws_security_group.maheshSG]
+vpc_security_group_ids = [aws_security_group.maheshSG.id]
 #iam_instance_profile = "mahesh-user"
  tags = {
    "Name"= "maheshec2"
